cbca58469a76bbbeeda39e5e7301c2a3f35cbd50
xujiang <xujiang156@qq.com>
2020-11-22 23:30:45 +0800

    :new: 跨域支持

--- server/src/middlewares/common.js
+++ server/src/middlewares/common.js
@@ -11,17 +11,21 @@ export const KoaBody = app => app.use(koaBody())
 
 // 配置跨域资源共享
 export const Cors = app => app.use(cors({
-    origin: function(ctx) {
-      if (ctx.url.indexOf('/api') > -1) {
-        return false;
-      }
-      return '*';
-    },
-    exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
-    maxAge: 5,
-    credentials: true,
-    allowMethods: ['GET'],
-    allowHeaders: ['Content-Type', 'Authorization', 'Accept', 'X-Requested-With'],
+  origin: function (ctx) {
+    const whiteList = [
+        'http://192.168.1.8:3000',
+        'http://192.168.3.15:8000'
+      ]; //可跨域白名单
+    if (whiteList.includes(ctx.request.header.origin) && ctx.url.indexOf(config.API_VERSION_PATH) > -1) {
+        return ctx.request.header.origin //注意，这里域名末尾不能带/，否则不成功，所以在之前我把/通过substr干掉了，允许来自指定域名请求, 如果设置为*，前端将获取不到错误的响应头
+    }
+    return ''
+  },
+  exposeHeaders: ['WWW-Authenticate', 'Server-Authorization', 'x-show-msg'],
+  maxAge: 5,  //  该字段可选，用来指定本次预检请求的有效期，单位为秒
+  credentials: true,
+  allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowHeaders: ['Content-Type', 'Authorization', 'Accept', 'X-Requested-With'],
   })
 )
 
